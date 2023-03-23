8b58d0bce8fde7c520e243ff3e4f77d0371ffdbc
Rufus Sam John Immanvel J <rufussamjohnimmanvelj@deloitte.com>
2023-03-23 15:22:06 +0530

    Reference poc

--- frontend/src/Components/EmployeeGrid/EmployeeGrid.js
+++ frontend/src/Components/EmployeeGrid/EmployeeGrid.js
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const EmployeeGrid = () => {
-  return <div>Hello</div>;
+  function checkUncheck() {
+    const selectAllCheckBox = document.getElementById("mainCheck");
+    const allCheckBox = document.getElementsByClassName("lang");
+    for (let i = 0; i < allCheckBox.length; i++) {
+      allCheckBox[i].checked = selectAllCheckBox.checked;
+    }
+  }
+
+  const employeesURL = "https://localhost:7243/api/Employee";
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    fetch(employeesURL)
+      .then((res) => res.json())
+      .then((data) => {
+        setEmployees(data);
+        console.log(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <div className="adminGrid">
+        <table>
+          <thead>
+            <tr>
+              <th>
+                <input type="checkbox" id="mainCheck" onClick={checkUncheck} />
+              </th>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Department</th>
+              <th>DateOfJoining</th>
+              <th>PhotoFileName</th>
+            </tr>
+          </thead>
+          <tbody>
+            {employees.map((e, i) => {
+              return (
+                <tr key={i}>
+                  <td>
+                    <input className="lang" type="checkbox" value={e.id} />
+                  </td>
+                  <td>{e.employeeId}</td>
+                  <td>{e.employeeName}</td>
+                  <td>{e.department}</td>
+                  <td>{e.dateOfJoining}</td>
+                  <td>{e.photoFileName}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default EmployeeGrid;
--- frontend/src/Screens/AdminHomePage.js
+++ frontend/src/Screens/AdminHomePage.js
@@ -3,6 +3,8 @@ import AdminGrid from "../Components/AdminGrid/AdminGrid";
 import EmployeeGrid from "../Components/EmployeeGrid/EmployeeGrid";
 import Header from "../Components/Header/Header";
 import "./AdminHomePage.css";
+import axios from "axios";
+import { v1 } from "uuid";
 
 const AdminHomePage = () => {
   const [adminGrid, setAdminGrid] = useState(true);
@@ -19,6 +21,50 @@ const AdminHomePage = () => {
   }
   useEffect(() => {}, [adminGrid, employeeGrid]);
 
+  async function addEmployee() {
+    const newEmployee = {
+      EmployeeID: v1(),
+      EmployeeName: "Batmanrop",
+      Department: "CSE",
+      DateofJoining: "01-01-0001",
+      PhotoFileName: "Check.jpg",
+      isActive: true,
+    };
+    try {
+      await axios.post("https://localhost:7243/api/Employee", newEmployee);
+    } catch (er) {
+      console.log(er);
+    }
+  }
+
+  async function deleteEmployee() {
+    const emp = {
+      EmployeeID: "F6D07690-C95A-11ED-B35B-CD1B78AF9A6F",
+    };
+    try {
+      await axios.post("https://localhost:7243/api/Employee/delete", emp);
+    } catch (er) {
+      console.log(er);
+    }
+  }
+
+  async function updateEmployee() {
+    const id = "F6D07690-C95A-11ED-B35B-CD1B78AF9A6F";
+    const updatedEmployee = {
+      EmployeeID: id,
+      EmployeeName: "Batmanrop",
+      Department: "AUDIT",
+      DateofJoining: "23-03-2021",
+      PhotoFileName: "jonDoe.jpg",
+      isActive: true,
+    };
+    try {
+      await axios.patch("https://localhost:7243/api/Employee", updatedEmployee);
+    } catch (er) {
+      console.log(er);
+    }
+  }
+
   return (
     <div>
       <Header />
@@ -27,14 +73,14 @@ const AdminHomePage = () => {
         <button onClick={handleEmployeeButton}>EmployeeGrid</button>
       </div>
       <div className="buttons">
-        <button className="button">
+        <button className="button" onClick={addEmployee}>
           <i className="fa-solid fa-plus"></i>ADD
         </button>
 
-        <button className="button">
+        <button className="button" onClick={deleteEmployee}>
           <i className="fa-solid fa-trash"></i>DELETE
         </button>
-        <button className="button">
+        <button className="button" onClick={updateEmployee}>
           <i className="fa-regular fa-pen-to-square"></i>UPDATE
         </button>
         <button className="button">
